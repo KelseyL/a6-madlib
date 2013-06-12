3c19771beba15db7562074c83ba445abdbca0db9
Kelsey Lawrence <kelseylawrence@Kelseys-MacBook-Pro.local>
2013-06-12 13:06:16 -0700

    closerr...

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,20 @@
 $(document).ready(function(){
-	//$("#madlibs").hide();
+	$("#madlibs").hide();
 
 	$('#button').click(function(event) {
+		$("#madlibs").show();
+
 		var adj1 = $('#adjective1').val();
 		var noun = $('#pluralnoun').val();
 		var body = $('#bodypart').val();
 		var adj2 = $('#adjective2').val();
 		var adj3 = $('#adjective3').val();
 
+		$('#input1').append(adj1);
+		$('#input2').append(noun);
+		$('#input3').append(body);
+		$('#input4').append(adj2);
+		$('#input5').append(adj3);
 
 	});
 
