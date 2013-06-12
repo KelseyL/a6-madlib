9b71a93a1e6ea3a38ec1067f5d43edd8f4f43323
Kelsey Lawrence <kelseylawrence@Kelseys-MacBook-Pro.local>
2013-06-12 12:32:31 -0700

    So far..
    Thanks Jason!!

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	//$("#madlibs").hide();
+
+	$('#button').click(function(event) {
+		var adj1 = $('#adjective1').val();
+		var noun = $('#pluralnoun').val();
+		var body = $('#bodypart').val();
+		var adj2 = $('#adjective2').val();
+		var adj3 = $('#adjective3').val();
+
+
+	});
+
+
+});
